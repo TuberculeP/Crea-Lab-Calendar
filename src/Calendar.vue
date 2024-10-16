519534f3a70e5073db80bb15596e0c14f365c075
MEHDI BELLAM <porcepic91@gmail.com>
2024-10-16 11:50:15 +0200

    Ajout de la branch Admin

--- src/Calendar.vue
+++ src/Calendar.vue
@@ -4,12 +4,8 @@ import "vue-cal/dist/vuecal.css";
 </script>
 
 <template>
-  <vue-cal
-    :time-from="8 * 60"
-    :time-to="19 * 60"
-    :time-step="30"
-    show-week-numbers
-    :disable-views="['years', 'year', 'month']"
-    style="height: 80vh; width: 80vw"
-  />
+  <vue-cal :time-from="8 * 60" :time-to="19 * 60" :time-step="30" show-week-numbers
+    :disable-views="['years', 'year', 'month']" style="height: 80vh; width: 80vw" />
 </template>
+
+
