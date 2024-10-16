a4bd1d9622b7f2bad7f54acb2f0fc661d5338bd0
Félix Laviéville <felix@oplit.fr>
2024-10-16 14:08:00 +0200

    WIP styling

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
 import Calendar from "./Calendar.vue";
+import CalButton from "./components/homemade/CalButton.vue";
 </script>
 
 <template>
   <Calendar />
+  <cal-button disabled>Click me</cal-button>
 </template>
---
+++ src/components/homemade/CalButton.vue
@@ -0,0 +1,13 @@
+<template>
+  <button v-bind="props" />
+</template>
+
+<script setup lang="ts">
+const props = defineProps();
+</script>
+
+<style scoped>
+button {
+  background-color: var(--color-vibrant-orange);
+}
+</style>
