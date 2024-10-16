fc9b18535eabeaeb5d511fa3944e04177f2e0953
Lucas Charoing <charoing.lucas@gmail.com>
2024-10-16 11:59:09 +0200

    Add machines select component

--- src/Calendar.vue
+++ src/Calendar.vue
@@ -1,13 +1,10 @@
 <script setup lang="ts">
-import { ref, watch } from "vue";
+import { ref } from "vue";
 import VueCal from "vue-cal";
 import "vue-cal/dist/vuecal.css";
+import { machines } from "./configs/machines";
 
 const activeView = ref("week");``
-
-watch(activeView, (newVal) => {
-  console.log(newVal);
-});
 </script>
 
 <template>
@@ -18,11 +15,7 @@ watch(activeView, (newVal) => {
     show-week-numbers
     :disable-views="['years', 'year', 'month']"
     style="height: 80vh; width: 80vw"
-    :splitDays="activeView === 'day' ? [
-      { id: 1, class: 'room', label: 'Salle' },
-      { id: 2, class: 'printer-3d', label: 'Imprimante 3D' },
-      { id: 3, class: 'sewing-machine', label: 'Machine à coudre' }
-    ] : undefined"
+    :splitDays="activeView === 'day' ? machines : undefined"
     v-model:active-view="activeView"
   />
 </template>
