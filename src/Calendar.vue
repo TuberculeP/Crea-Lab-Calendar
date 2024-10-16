306dd25c594e27e379525bc1ef842adae7b4ffb5
Lucas Charoing <charoing.lucas@gmail.com>
2024-10-16 11:34:07 +0200

    Add colors to machine sections

--- src/Calendar.vue
+++ src/Calendar.vue
@@ -20,9 +20,21 @@ watch(activeView, (newVal) => {
     style="height: 80vh; width: 80vw"
     :splitDays="activeView === 'day' ? [
       { id: 1, class: 'room', label: 'Salle' },
-      { id: 2, class: '3d-printer', label: 'Imprimante 3D' },
+      { id: 2, class: 'printer-3d', label: 'Imprimante 3D' },
       { id: 3, class: 'sewing-machine', label: 'Machine à coudre' }
     ] : undefined"
     v-model:active-view="activeView"
   />
 </template>
+
+<style>
+.vuecal__cell-split.room {
+  background-color: rgba(0, 128, 255, 0.1);
+}
+.vuecal__cell-split.printer-3d {
+  background-color: rgba(255, 0, 212, 0.1);
+}
+.vuecal__cell-split.sewing-machine {
+  background-color: rgba(0, 255, 136, 0.1);
+}
+</style>
