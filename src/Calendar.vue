921b1c58d60ca6a32e4ae71e3eb25a10fb68dc43
Lucas Charoing <charoing.lucas@gmail.com>
2024-10-16 11:28:38 +0200

    Add machines sections to day tab

--- src/Calendar.vue
+++ src/Calendar.vue
@@ -1,6 +1,13 @@
 <script setup lang="ts">
+import { ref, watch } from "vue";
 import VueCal from "vue-cal";
 import "vue-cal/dist/vuecal.css";
+
+const activeView = ref("week");``
+
+watch(activeView, (newVal) => {
+  console.log(newVal);
+});
 </script>
 
 <template>
@@ -11,5 +18,11 @@ import "vue-cal/dist/vuecal.css";
     show-week-numbers
     :disable-views="['years', 'year', 'month']"
     style="height: 80vh; width: 80vw"
+    :splitDays="activeView === 'day' ? [
+      { id: 1, class: 'room', label: 'Salle' },
+      { id: 2, class: '3d-printer', label: 'Imprimante 3D' },
+      { id: 3, class: 'sewing-machine', label: 'Machine à coudre' }
+    ] : undefined"
+    v-model:active-view="activeView"
   />
 </template>
