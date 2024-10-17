1482a0e334278d7ab10adc344d0264f55f026de8
Fanny Gautier <fannygautier92@gmail.com>
2024-10-17 14:27:18 +0200

    feat(events): add modal, loader, new utils

---
+++ src/components/Calendar/PreviewModal.vue
@@ -0,0 +1,205 @@
+<script setup>
+import { defineProps } from 'vue';
+import {
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogOverlay,
+  DialogPortal,
+  DialogRoot,
+  DialogTitle,
+  DialogTrigger,
+} from 'radix-vue'
+
+
+const props = defineProps({
+  selectedEvent: Object,
+  showDialog: Boolean,
+  machines: Array,
+});
+
+</script>
+<template>
+<DialogRoot v-model:open="props.showDialog">
+    <DialogOverlay class="DialogOverlay" />
+    <DialogPortal>
+      <DialogContent class="DialogContent">
+        <DialogTitle class="DialogTitle">
+          <h1>
+          {{ props.selectedEvent.title }}
+          </h1>
+        </DialogTitle>
+        <DialogDescription class="DialogDescription">
+
+        <div v-if="selectedEvent.isMachineSlot">
+          <h3>Machine:</h3>
+          <p>{{ props.machines.find((machine) => machine.id === selectedEvent.split).label }}</p>
+       
+        </div>
+
+        <h3>Description:</h3>
+        <p>{{ props.selectedEvent.description }}</p>
+
+        <div v-if="props.selectedEvent.assignee.length > 0">
+          <h3>Participants :</h3> 
+          <div
+          class="assignees-on-card" 
+          v-for="
+            assignee in props.selectedEvent.assignee
+          ">
+            <div class="user-tag">
+              {{ assignee.directus_users_id.first_name[0] }}{{ assignee.directus_users_id.last_name[0] }}
+            </div>
+            <p>{{ assignee.directus_users_id.email }}</p>
+          </div>
+        </div>
+        <div>
+          <h3>Date de début :</h3>
+          <p>{{ new Date(props.selectedEvent.start).toLocaleDateString() }}</p>
+
+          <h3>Date de fin :</h3>
+          <p>{{ new Date(props.selectedEvent.end).toLocaleDateString()  }}</p>
+        </div>
+        </DialogDescription>
+        <DialogClose as-child>
+          <button
+            class="Button green"
+            @click="showDialog = false"
+          >
+            Close
+          </button>
+        </DialogClose>
+      </DialogContent>
+    </DialogPortal>
+  </DialogRoot>
+
+</template>
+
+<style scoped>
+/* reset */
+button,
+fieldset
+{
+  all: unset;
+}
+
+.assignees-on-card{
+  display: flex;
+  flex-direction: row;
+  gap: 4px;
+  align-items: center;
+  justify-content: start;
+}
+
+.DialogOverlay {
+  background-color: rgba(0, 0, 0, 0.9); /* var(--black-a9) replaced */
+  position: fixed;
+  inset: 0;
+  animation: overlayShow 150ms cubic-bezier(0.16, 1, 0.3, 1);
+  z-index: 999;
+}
+
+.DialogContent {
+  background-color: #ffffff;
+  border-radius: 6px;
+  box-shadow: rgba(0, 0, 0, 0.35) 0px 10px 38px -10px, rgba(0, 0, 0, 0.2) 0px 10px 20px -15px;
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 90vw;
+  max-width: 450px;
+  max-height: 85vh;
+  padding: 25px;
+  animation: contentShow 150ms cubic-bezier(0.16, 1, 0.3, 1);
+  z-index: 1000;
+}
+.DialogContent:focus {
+  outline: none;
+}
+
+.DialogTitle {
+  margin: 0;
+  font-weight: 500;
+  color: #1f2937; /* var(--mauve-12) replaced */
+  font-size: 17px;
+}
+
+.DialogDescription {
+  margin: 10px 0 20px;
+  color: #4b5563; /* var(--mauve-11) replaced */
+  font-size: 15px;
+  line-height: 1.5;
+}
+
+
+.title{
+  font-size: large;
+  padding: 2px;
+}
+
+.description{
+  font-size: small;
+  padding: 2px;
+}
+
+.event {
+  display: flex;
+  flex-direction: column;
+  gap: 1px;
+  justify-content: start;
+  align-items: start;
+}
+
+.event-cell {
+  display: flex;
+  flex-direction: column;
+  gap: 2px;
+  justify-content: start;
+  align-items: start;
+}
+
+.assignees {
+  display: flex;
+  flex-direction: row;
+  gap: 4px;
+  padding: 4px;
+  align-items: start;
+  justify-content: center;
+}
+
+.user-tag {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #f0f0f0;
+  color: #333;
+  font-size: small;
+  height: 10px;
+  width: 10px;
+  border-radius: 100%;
+  padding: 6px;
+  cursor: pointer;
+}
+
+.tooltip {
+  position: absolute;
+  background-color: #333;
+  color: #fff;
+  padding: 5px 10px;
+  border-radius: 4px;
+  font-size: 14px;
+  z-index: 1000;
+  pointer-events: none;
+}
+
+.vuecal__cell-split.room {
+  background-color: rgba(0, 128, 255, 0.1);
+}
+.vuecal__cell-split.printer-3d {
+  background-color: rgba(255, 0, 212, 0.1);
+}
+.vuecal__cell-split.sewing-machine {
+  background-color: rgba(0, 255, 136, 0.1);
+}
+</style>
